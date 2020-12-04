fe44e1d3d85d3279547a67ee03a20f723608843b
Alex Eberts <alex.eberts@gmail.com>
2020-12-04 08:59:54 -0800

    Added semantic-ui and started prototyping

--- src/shared/com/example/workspaces/fulcro_demo_cards.cljs
+++ src/shared/com/example/workspaces/fulcro_demo_cards.cljs
@@ -1,9 +1,11 @@
 (ns com.example.workspaces.fulcro-demo-cards
   (:require [com.fulcrologic.fulcro.components :as fp]
             [com.fulcrologic.fulcro-css.localized-dom :as dom]
+            [com.fulcrologic.semantic-ui.modules.dropdown.ui-dropdown :refer [ui-dropdown]]
+            [com.fulcrologic.semantic-ui.modules.dropdown.ui-dropdown-menu :refer [ui-dropdown-menu]]
+            [com.fulcrologic.semantic-ui.modules.dropdown.ui-dropdown-item :refer [ui-dropdown-item]]
             [nubank.workspaces.core :as ws]
             [nubank.workspaces.card-types.fulcro3 :as ct.fulcro]
-   ;;[nubank.workspaces.lib.fulcro-portal :as f.portal]
             [com.fulcrologic.fulcro.mutations :as fm]))
 
 (fp/defsc FulcroDemo
@@ -12,10 +14,10 @@
    :ident         (fn [] [::id "singleton"])
    :query         [:counter]}
   (dom/div
-    (str "Fulcro counter demo [" counter "]")
-    (dom/button {:onClick #(fm/set-value! this :counter (inc counter))} "+")))
+   (str "Fulcro counter demo [" counter "]")
+   (dom/button {:onClick #(fm/set-value! this :counter (inc counter))} "+")))
 
 (ws/defcard fulcro-demo-card
-  (ct.fulcro/fulcro-card
-    {::ct.fulcro/root FulcroDemo
-     ::ct.fulcro/wrap-root? true}))
+            (ct.fulcro/fulcro-card
+             {::ct.fulcro/root       FulcroDemo
+              ::ct.fulcro/wrap-root? true}))
